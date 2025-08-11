c58d4eaa949b84f2f44758eaf3e77c8fad630eb6
DEEPAN <141644376+deepanmpc@users.noreply.github.com>
2025-08-11 23:17:07 +0530

    Set BrowserRouter basename to /cicd-frontend for GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+  <BrowserRouter basename="/cicd-frontend">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<Aboutus />} />
